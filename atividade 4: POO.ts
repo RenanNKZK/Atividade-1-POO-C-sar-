9a3fb08e881ac1dedcc4971495faf0c0a942bde9
RenanNKZK <renan.nakazaki@escola.pr.gov.br>
2026-09-10 14:40:06 -0300

    Create atividade 4: POO.ts

---
+++ atividade 4: POO.ts
@@ -0,0 +1,11 @@
+class Retangulo{
+  largura: number;
+  altura: number;
+
+  constructor(altura:number,largura:number){
+    this.altura= altura;
+    this.largura= largura;
+  }
+  calcularperimetro()
+  return.this
+}
